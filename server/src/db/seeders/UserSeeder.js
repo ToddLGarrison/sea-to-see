91892797d2c704176f4d126c91906a482bb0b738
Todd Garrison <toddlgarrison@gmail.com>
2023-05-15 16:59:41 -0400

    Add UserSeeder

---
+++ server/src/db/seeders/UserSeeder.js
@@ -0,0 +1,31 @@
+import { User } from "../../models/index.js"
+
+class UserSeeder {
+    static async seed() {
+        const userData = [
+            {
+                username: "Bilbo",
+                email: "bilbo@baggins.com",
+                password: "1ring",
+            },
+            {
+                username: "Geralt",
+                email: "g-money@email.com",
+                password: "roach",
+            },
+            {
+                username: "Carrots",
+                email: "carrot@email.com",
+                password: "12345",
+            }
+        ]
+        for (const singleUserData of usersData) {
+            const currentUser = await User.query().findOne({ email: singleUserData.email })
+            if (!currentUser) {
+                await User.query().insert(singleUserData)
+            }
+        }
+    }
+}
+
+export default UserSeeder
