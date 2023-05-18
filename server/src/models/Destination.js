119d5064abdfb6648ab1036972ccf5116c0e57a4
Todd Garrison <toddlgarrison@gmail.com>
2023-05-18 15:38:25 -0400

    Edit DestinationSeeder

--- server/src/models/Destination.js
+++ server/src/models/Destination.js
@@ -6,7 +6,7 @@ class Destination extends Model {
     }
 
     static get relationMappings(){
-        const { User, Itinerary } = require("./index.js")
+        const { Itinerary } = require("./index.js")
 
         return {
             itinerary: {
