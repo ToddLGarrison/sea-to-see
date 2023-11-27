86f65b93ac5d07fee074e9c699ebd2c52c5af045
Todd Garrison <toddlgarrison@gmail.com>
2023-11-27 16:41:53 -0500

    update text

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,12 +1,14 @@
 import React from "react";
 import taj from "../assets/images/baby-taj.png"
 
+const currentDate = new Date().getFullYear();
+
 const HomePage = (props) => {
     return(
         <div className="primary home-box">
             <div className="grid homepage">
                 <img className="homepage-photo" src={taj} alt="A person standing in front of the baby taj" />
-                <p className="developed-by-text">Developed by Todd Garrison</p>
+                <p className="developed-by-text">Developed by Todd Garrison | Copyright {currentDate}</p>
             </div>
             <h4 className="homepage-text">
                 Plan <em>your</em> trip
